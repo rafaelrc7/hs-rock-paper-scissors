3a07a4f645f5e10b1c50f2dbb1feccdd4e228f9a
rafaelrc7 <rafaelrc7@gmail.com>
2022-03-14 23:43:07 -0300

    Enum improvements

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,16 @@
 module Main where
 
-import System.Random (randomRIO)
+import System.Random (randomRIO, Random(randomR, random))
 import Text.Read (readMaybe)
 
 data Move = Rock | Paper | Scissors
-  deriving (Show, Enum)
+  deriving (Show, Enum, Bounded)
+
+instance Random Move where
+  randomR (a, b) g = case randomR (fromEnum a, fromEnum b) g of
+                        (x, g') -> (toEnum x, g')
+  random g = randomR (minBound, maxBound) g
+
 
 data Result = Draw | Win | Lose
   deriving (Show, Enum)
@@ -14,13 +20,13 @@ main = putStrLn "Welcome to Rock, Paper, Scissors"
          >> playGame
 
 playGame :: IO ()
-playGame = putStrLn "Type 0 for Rock, 1 for Paper, 2 for Scissors or 3 to quit."
+playGame = putStrLn "\nType 0 for Rock, 1 for Paper, 2 for Scissors or 3 to quit."
             >> getLine >>= \lineIn ->
-              case readMaybe lineIn of
-                Just 3 -> putStrLn "Thanks for playing." >> return ()
-                Just n
-                  | n >= 0 && n <= 2 -> playAI (toEnum n) >> playGame
-                _ -> putStrLn "Invalid input." >> playGame
+               case readMaybe lineIn of
+                 Just 3 -> putStrLn "Thanks for playing." >> return ()
+                 Just n
+                   | n >= fromEnum (minBound :: Move) && n <= fromEnum (maxBound :: Move) -> playAI (toEnum n) >> playGame
+                 _ -> putStrLn "Invalid input." >> playGame
 
 play :: Move -> Move -> Result
 play move1 move2 = toEnum $ (move1' - move2') `mod` 3
@@ -28,11 +34,12 @@ play move1 move2 = toEnum $ (move1' - move2') `mod` 3
         move2' = fromEnum move2
 
 randomMove :: IO Move
-randomMove = toEnum <$> randomRIO (0, 2)
+randomMove = randomRIO (minBound, maxBound)
 
 playAI :: Move -> IO ()
 playAI playerMove = randomMove >>= \aiMove ->
-                      putStrLn $ "Your move was: " <> show playerMove
-                          <> "\nThe computer move was:" <> show aiMove
-                          <> "\nThe result is a: " <> (show $ play playerMove aiMove)
+                      let result = play playerMove aiMove
+                      in putStrLn $ "Your move was: " <> show playerMove
+                                 <> "\nThe computer move was:" <> show aiMove
+                                 <> "\nThe result is a: " <> show result
 
